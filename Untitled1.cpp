4ef6a3d50cbc94bc08a8e1e83754d5e4d56c6935
yopantumorang <ekoyopan13@gmail.com>
2019-09-07 12:56:22 +0700

    update lebih baru

---
+++ Untitled1.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+int main(){
+	cout<<"hello world";
+	//lihatlah luka ini yang sakitnya abadi yang terbalut hangatnya bekas pelukmu aku tak akan lupa tak akn pernah bisa tentang apa yang harus memisahkan kita
+}
