cadd33e74b463116dea8a04cdd3ac1b685ec1a9c
yopantumorang <ekoyopan13@gmail.com>
2019-09-09 10:51:05 +0700

    coba ulang

---
+++ ayam/src/Mahasiswa.java
@@ -0,0 +1,18 @@
+public class Mahasiswa {
+  int nip;
+  String nama;
+  
+  public Mahasiswa(int i, String n){
+      nip=i;
+      nama=n;
+  }
+  public int getNRP(){
+      
+  return nip;
+  }
+  public String getNama(){
+      
+  return nama;
+  }
+
+}
---
+++ ayam/src/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String args[]){
+        Mahasiswa mhs=new Mahasiswa(12345,"Budi mikarti");
+        System.out.println("NRP: "+mhs.getNRP());
+        System.out.println("Nama: "+mhs.getNama());
+    }
+
+}
